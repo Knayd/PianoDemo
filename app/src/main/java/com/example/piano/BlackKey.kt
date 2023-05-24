6369001f636591a9df2c01b5b7a9d06abe26ad21
Deryan Argueta <deryan@ravn.co>
2023-05-23 23:12:43 -0600

    Adding custom keys

---
+++ app/src/main/java/com/example/piano/BlackKey.kt
@@ -0,0 +1,87 @@
+package com.example.piano
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.piano.ui.theme.PianoTheme
+
+@Composable
+fun BlackKey(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val borderColor = Color(if (isPressed) 0xFF313436 else 0xFF3A3D40)
+
+    val backgroundGradient = if (isPressed) listOf(
+        Color(0xFF747577),
+        Color(0xFF313436),
+    ) else listOf(
+        Color(0xFF878A8C),
+        Color(0xFF3A3D40),
+    )
+
+    val embossGradient = if (isPressed) listOf(
+        Color(0xFF0C0D0E),
+        Color(0xFF2C2E31)
+    ) else listOf(
+        Color(0xFF0D0F10),
+        Color(0xFF3A3D40)
+    )
+
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .offset(y = (-2).dp)
+            .border(1.dp, borderColor, RoundedCornerShape(bottomStart = 3.dp, bottomEnd = 3.dp))
+            .clickable(interactionSource = interactionSource, indication = null) { }
+            .width(46.dp)
+    )
+    {
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .height(22.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(bottomEnd = 3.dp, bottomStart = 3.dp))
+                .background(Brush.verticalGradient(embossGradient))
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(y = if (isPressed) (-7).dp else (-10).dp)
+                .clip(RoundedCornerShape(bottomStart = 15.dp, bottomEnd = 15.dp))
+                .background(Brush.verticalGradient(backgroundGradient))
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun BlackKeyPreview() {
+    PianoTheme {
+        BlackKey()
+    }
+}
---
+++ app/src/main/java/com/example/piano/WhiteKey.kt
@@ -0,0 +1,112 @@
+package com.example.piano
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import com.example.piano.ui.theme.PianoTheme
+
+@Composable
+fun WhiteKey(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val borderGradient = if (isPressed) listOf(
+        Color(0xFFAFAFAF),
+        Color(0xFFD5D5D5)
+    ) else listOf(
+        Color(0xFFCDCDCD),
+        Color(0xFFFAFAFA)
+    )
+
+    val backgroundGradient = if (isPressed) listOf(
+        Color(0xFFC2C2C2),
+        Color(0xFFD9D9D9),
+    ) else listOf(
+        Color(0xFFE4E4E4),
+        Color(0xFFFFFFFF),
+    )
+
+    val embossGradient = if (isPressed) listOf(
+        Color(0xFFB9B9B9),
+        Color(0xFFD6D6D6)
+    ) else listOf(
+        Color(0xFFD9D9D9),
+        Color(0xFFFFFFFF)
+    )
+
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .clickable(interactionSource = interactionSource, indication = null) { }
+            .width(48.dp)
+    )
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .zIndex(1f)
+                .clip(RoundedCornerShape(3.dp))
+                .background(Brush.verticalGradient(backgroundGradient))
+                .border(
+                    width = 1.dp,
+                    brush = Brush.verticalGradient(borderGradient),
+                    shape = RoundedCornerShape(3.dp)
+                )
+        ) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 8.dp),
+                text = "A0",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Black
+            )
+        }
+        Box(
+            modifier = Modifier
+                .height(if (isPressed) 12.dp else 17.dp)
+                .fillMaxWidth()
+                .offset(y = (-3).dp)
+                .clip(RoundedCornerShape(bottomEnd = 3.dp, bottomStart = 3.dp))
+                .background(Brush.verticalGradient(embossGradient))
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun WhiteKeyPreview() {
+    PianoTheme {
+        WhiteKey()
+    }
+}
